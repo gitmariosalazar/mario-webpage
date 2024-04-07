f223b10513d3b1e5084d3bdb8e0557da71acbc82
Mario Salazar <mariosalazar.ms.10@gmail.com>
2024-04-07 09:24:49 -0500

    React Project, upload by Mario Salazar fiveth time

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,108 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { Sidebar } from "primereact/sidebar";
 import { Button } from "primereact/button";
 import { Avatar } from "primereact/avatar";
-import { StyleClass } from "primereact/StyleClass";
-
+import { PanelMenu } from "primereact/panelmenu";
+import { Badge } from "primereact/badge";
 import "./css/sidebar.css";
 import avatar_logo from "./img/LogoMario.png";
 import mario from "./img/MarioSalazar.jpeg";
 
 export default function HeadlessDemo() {
   const [visible, setVisible] = useState(false);
-  const btnRef1 = useRef(null);
-  const btnRef2 = useRef(null);
-  const btnRef3 = useRef(null);
-  const btnRef4 = useRef(null);
+
+  const itemRenderer = (item, options) => (
+    <a
+      className="flex align-items-center px-3 py-2 cursor-pointer"
+      onClick={options.onClick}
+    >
+      <span className={`${item.icon} text-primary`} />
+      <span className={`mx-2 ${item.items && "font-semibold"}`}>
+        {item.label}
+      </span>
+      {item.badge && <Badge className="ml-auto" value={item.badge} />}
+      {item.shortcut && (
+        <span className="ml-auto border-1 surface-border border-round surface-100 text-xs p-1">
+          {item.shortcut}
+        </span>
+      )}
+    </a>
+  );
+
+  const items = [
+    {
+      label: "Mail",
+      icon: "pi pi-envelope",
+      badge: 5,
+      template: itemRenderer,
+      items: [
+        {
+          label: "Compose",
+          icon: "pi pi-file-edit",
+          shortcut: "⌘+N",
+          template: itemRenderer,
+        },
+        {
+          label: "Inbox",
+          icon: "pi pi-inbox",
+          badge: 5,
+          template: itemRenderer,
+        },
+        {
+          label: "Sent",
+          icon: "pi pi-send",
+          shortcut: "⌘+S",
+          template: itemRenderer,
+        },
+        {
+          label: "Trash",
+          icon: "pi pi-trash",
+          shortcut: "⌘+T",
+          template: itemRenderer,
+        },
+      ],
+    },
+    {
+      label: "Reports",
+      icon: "pi pi-chart-bar",
+      shortcut: "⌘+R",
+      template: itemRenderer,
+      items: [
+        {
+          label: "Sales",
+          icon: "pi pi-chart-line",
+          badge: 3,
+          template: itemRenderer,
+        },
+        {
+          label: "Products",
+          icon: "pi pi-list",
+          badge: 6,
+          template: itemRenderer,
+        },
+      ],
+    },
+    {
+      label: "Profile",
+      icon: "pi pi-user",
+      shortcut: "⌘+W",
+      template: itemRenderer,
+      items: [
+        {
+          label: "Settings",
+          icon: "pi pi-cog",
+          shortcut: "⌘+O",
+          template: itemRenderer,
+        },
+        {
+          label: "Privacy",
+          icon: "pi pi-shield",
+          shortcut: "⌘+P",
+          template: itemRenderer,
+        },
+      ],
+    },
+  ];
 
   return (
     <div className="card flex justify-content-center">
@@ -55,174 +144,9 @@ export default function HeadlessDemo() {
                     ></Button>
                   </span>
                 </div>
-
                 <hr className="mb-3 mx-3 border-top-3 border-none surface-border" />
-
-                <div className="overflow-y-auto">
-                  <ul className="list-none p-3 m-0">
-                    <li>
-                      <StyleClass
-                        nodeRef={btnRef1}
-                        selector="@next"
-                        enterClassName="hidden"
-                        enterActiveClassName="slidedown"
-                        leaveToClassName="hidden"
-                        leaveActiveClassName="slideup"
-                      >
-                        <div
-                          ref={btnRef1}
-                          className="p-ripple p-3 flex align-items-center justify-content-between text-600 cursor-pointer"
-                        >
-                          <span className="font-medium">FAVORITES</span>
-                          <i className="pi pi-chevron-down"></i>
-                        </div>
-                      </StyleClass>
-                      <ul className="list-none p-0 m-0 overflow-hidden">
-                        <li>
-                          <a className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full">
-                            <i className="pi pi-home mr-2"></i>
-                            <span className="font-medium">Dashboard</span>
-                          </a>
-                        </li>
-                        <li>
-                          <a className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full">
-                            <i className="pi pi-bookmark mr-2"></i>
-                            <span className="font-medium">Bookmarks</span>
-                          </a>
-                        </li>
-                        <li>
-                          <StyleClass
-                            nodeRef={btnRef2}
-                            selector="@next"
-                            enterClassName="hidden"
-                            enterActiveClassName="slidedown"
-                            leaveToClassName="hidden"
-                            leaveActiveClassName="slideup"
-                          >
-                            <a
-                              ref={btnRef2}
-                              className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full"
-                            >
-                              <i className="pi pi-chart-line mr-2"></i>
-                              <span className="font-medium">Reports</span>
-                              <i className="pi pi-chevron-down ml-auto mr-1"></i>
-                            </a>
-                          </StyleClass>
-                          <ul className="list-none py-0 pl-3 pr-0 m-0 hidden overflow-y-hidden transition-all transition-duration-400 transition-ease-in-out">
-                            <li>
-                              <StyleClass
-                                nodeRef={btnRef3}
-                                selector="@next"
-                                enterClassName="hidden"
-                                enterActiveClassName="slidedown"
-                                leaveToClassName="hidden"
-                                leaveActiveClassName="slideup"
-                              >
-                                <a
-                                  ref={btnRef3}
-                                  className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full"
-                                >
-                                  <i className="pi pi-chart-line mr-2"></i>
-                                  <span className="font-medium">Revenue</span>
-                                  <i className="pi pi-chevron-down ml-auto mr-1"></i>
-                                </a>
-                              </StyleClass>
-                              <ul className="list-none py-0 pl-3 pr-0 m-0 hidden overflow-y-hidden transition-all transition-duration-400 transition-ease-in-out">
-                                <li>
-                                  <a className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full">
-                                    <i className="pi pi-table mr-2"></i>
-                                    <span className="font-medium">View</span>
-                                  </a>
-                                </li>
-                                <li>
-                                  <a className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full">
-                                    <i className="pi pi-search mr-2"></i>
-                                    <span className="font-medium">Search</span>
-                                  </a>
-                                </li>
-                              </ul>
-                            </li>
-                            <li>
-                              <a className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full">
-                                <i className="pi pi-chart-line mr-2"></i>
-                                <span className="font-medium">Expenses</span>
-                              </a>
-                            </li>
-                          </ul>
-                        </li>
-                        <li>
-                          <a className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full">
-                            <i className="pi pi-users mr-2"></i>
-                            <span className="font-medium">Team</span>
-                          </a>
-                        </li>
-                        <li>
-                          <a className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full">
-                            <i className="pi pi-comments mr-2"></i>
-                            <span className="font-medium">Messages</span>
-                            <span
-                              className="inline-flex align-items-center justify-content-center ml-auto bg-blue-500 text-0 border-circle"
-                              style={{ minWidth: "1.5rem", height: "1.5rem" }}
-                            >
-                              3
-                            </span>
-                          </a>
-                        </li>
-                        <li>
-                          <a className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full">
-                            <i className="pi pi-calendar mr-2"></i>
-                            <span className="font-medium">Calendar</span>
-                          </a>
-                        </li>
-                        <li>
-                          <a className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full">
-                            <i className="pi pi-cog mr-2"></i>
-                            <span className="font-medium">Settings</span>
-                          </a>
-                        </li>
-                      </ul>
-                    </li>
-                  </ul>
-                  <ul className="list-none p-3 m-0">
-                    <li>
-                      <StyleClass
-                        nodeRef={btnRef4}
-                        selector="@next"
-                        enterClassName="hidden"
-                        enterActiveClassName="slidedown"
-                        leaveToClassName="hidden"
-                        leaveActiveClassName="slideup"
-                      >
-                        <div
-                          ref={btnRef4}
-                          className="p-ripple p-3 flex align-items-center justify-content-between text-600 cursor-pointer"
-                        >
-                          <span className="font-medium">APPLICATION</span>
-                          <i className="pi pi-chevron-down"></i>
-                        </div>
-                      </StyleClass>
-                      <ul className="list-none p-0 m-0 overflow-hidden">
-                        <li>
-                          <a className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full">
-                            <i className="pi pi-folder mr-2"></i>
-                            <span className="font-medium">Projects</span>
-                          </a>
-                        </li>
-                        <li>
-                          <a className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full">
-                            <i className="pi pi-chart-bar mr-2"></i>
-                            <span className="font-medium">Performance</span>
-                          </a>
-                        </li>
-                        <li>
-                          <a className="p-ripple flex align-items-center cursor-pointer p-3 border-round text-700 hover:surface-100 transition-duration-150 transition-colors w-full">
-                            <i className="pi pi-cog mr-2"></i>
-                            <span className="font-medium">Settings</span>
-                          </a>
-                        </li>
-                      </ul>
-                    </li>
-                  </ul>
+                <div className="card flex justify-content-center sidebar">
+                  <PanelMenu model={items} className="w-full md:w-20rem" />
                 </div>
                 <div className="mt-auto">
                   <hr className="mb-3 mx-3 border-top-3 border-none surface-border" />
